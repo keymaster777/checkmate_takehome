23e7cfb129e17eeee09c40b5ef9b8ce970623e3e
Riley <rileykeycleary@gmail.com>
2026-07-07 15:13:08 -0600

    Adds validation to params and order items

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,15 +3,44 @@ class Api::OrdersController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        order = Order.create!
+        return unless validate_items_param
 
-        params[:items].each do |item|
-            order.order_items.create!(
-            menu_item_id: item[:item_id],
-            qty: item[:qty]
-            )
+        order = nil
+
+        begin
+        Order.transaction do
+            order = Order.create!
+
+            params[:items].each do |item|
+                menu_item = MenuItem.find(item[:item_id])
+
+                order.order_items.create!(
+                    menu_item: menu_item,
+                    qty: item[:qty]
+                )
+            end
         end
 
         render json: order, status: :created
+
+        # Catching any active record error, we may want to handle other errors differently
+        rescue ActiveRecord::ActiveRecordError => e
+            render json: {
+                error: e.message,
+            }, status: :bad_request
+        end
+    end
+
+    private
+
+    def validate_items_param
+        if params[:items].blank?
+            render json: {
+            error: "items is required and cannot be empty"
+            }, status: :bad_request
+
+            return false
+        end
+        true
     end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :menu_item
+
+  validates :qty, numericality: { only_integer: true, greater_than: 0 }
 end
